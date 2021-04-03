ff2f93d675ba499f0e7c9d9ff51a854b79216919
Arjit Chaudhary <arjit.c@gmail.com>
2021-04-04 00:49:17 +0530

    Update securitytrails.php

--- securitytrails.php
+++ securitytrails.php
@@ -299,5 +299,29 @@ class SecurityTrails {
 		
 	}
 
+	function searchWithDSL($query) {
+		
+		$curl = curl_init();
+
+		curl_setopt_array($curl, [
+			CURLOPT_URL => "https://api.securitytrails.com/v1/ips/list",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "{\"query\":\"$query\"}",
+			CURLOPT_HTTPHEADER => [
+				"apikey: $this->APIKey",
+				"Content-Type: application/json"
+			],
+		]);
+
+		$response = curl_exec($curl);
+		curl_close($curl);
+		return $response;
+	}
+
 }
 ?>
